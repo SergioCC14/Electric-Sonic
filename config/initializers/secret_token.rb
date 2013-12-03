1de41e680b8c4bc5d90c499ed1c9d43dc3a3296a
SergioCC14 <sergio.canis@outlook.com>
2013-12-03 18:02:49 +0100

    Primer Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ElectricSonic::Application.config.secret_token = 'aa3b2eb85ec9f0f8fbb07f96a8f8cacd0b8c9edd7382eeba43fc7c29667d958ddb457803c0faa7a4a272fc2971f6c6592fa7e9b46bff552fcb1dd39e52445a3e'

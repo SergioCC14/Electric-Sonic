d4854f7c09381bfaee54ae9e76e96925afd69d76
SergioCC14 <sergio.canis@outlook.com>
2014-01-05 13:28:20 +0100

    Flujos de control
     - Ahora el formulario de Tribunal (y grupos) no se envía si no hay
    profesores (o alumnos) seleccionados
     - He dejado más puro el Listado 1 cuando no hay modelos
     - Ayuda en Login

--- app/models/tfg.rb
+++ app/models/tfg.rb
@@ -2,8 +2,8 @@ class Tfg < ActiveRecord::Base
 
   attr_accessible :tema, :fecha_comienzo, :alumno_id
 
-  belongs_to :alumno
+  belongs_to :alumno, :dependent => :destroy
 
-  has_one :tribunal
+  has_one :tribunal, :dependent => :destroy
 
 end
